a5057f45423b462317c23a3d5f33931df9c12b19
yangchen <1277687167@qq.com>
2024-02-23 17:12:18 +0800

    333

--- src/main/java/com/atguigu/git/TestGit.java
+++ src/main/java/com/atguigu/git/TestGit.java
@@ -4,5 +4,6 @@ public class TestGit {
     public static void main(String[] args) {
         System.out.println("git");
         System.out.println("version2");
+        System.out.println("version3");
     }
 }
